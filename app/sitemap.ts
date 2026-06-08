2164b4a13991db2fb06d41baf3ea602f45a8b7fa
Vicky Kumawat <vickykmt16@gmail.com>
2026-06-08 12:30:49 +0530

    Fix sitemap domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,21 +3,21 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://pg-finder-ruddy.vercel.app',
+      url: 'https://pg-finder-sikar.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
-      url: 'https://pg-finder-ruddy.vercel.app/about',
+      url: 'https://pg-finder-sikar.vercel.app/about',
       lastModified: new Date(),
     },
     {
-      url: 'https://pg-finder-ruddy.vercel.app/search',
+      url: 'https://pg-finder-sikar.vercel.app/search',
       lastModified: new Date(),
     },
     {
-      url: 'https://pg-finder-ruddy.vercel.app/reviews',
+      url: 'https://pg-finder-sikar.vercel.app/reviews',
       lastModified: new Date(),
     },
   ]
